c3151d804a0eda17d2042f81f8b9979c655d4cae
Antony Jones <aj@desirableobjects.co.uk>
2019-08-21 22:26:49 +0100

    update various things

--- test/setup-jest.js
+++ test/setup-jest.js
@@ -1,9 +1,1 @@
-import config from 'config'
-
-Object
-  .entries(config)
-  .forEach(n => {
-    process.env[n[0]] = n[1]
-  })
-
-  global.fetch = require('node-fetch')
+global.fetch = require('node-fetch')
